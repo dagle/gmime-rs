567ae12be7047a7a6412e50669db9a472c827dce
Per Odlund <per.odlund@gmail.com>
2023-03-24 17:42:24 +0100

    Implement read for streams.

--- src/stream.rs
+++ src/stream.rs
@@ -1,10 +1,46 @@
+use std::convert::TryInto;
+use std::io::ErrorKind::WriteZero;
+use std::io::{Read, Error, Write};
+
 use crate::Stream;
+use crate::traits::StreamExt;
 use glib::object::IsA;
 use glib::translate::*;
 
 pub trait StreamExtManual: 'static {
     #[doc(alias = "g_mime_stream_write")]
     fn write(&self, buf: &[u8]) -> isize;
+}
+
+impl Write for Stream {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let size = StreamExtManual::write(self, buf);
+        if size >= 0 {
+            Ok(size.try_into().unwrap())
+        } else {
+            Err(Error::new(WriteZero, "Couldn't read from stream"))
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        let size = StreamExt::flush(self);
+        if size < 0 {
+            Err(Error::new(WriteZero, "Couldn't flush stream"))
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl Read for Stream {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let size = StreamExt::read(self, buf);
+        if size >= 0 {
+            Ok(size.try_into().unwrap())
+        } else {
+            Err(Error::new(WriteZero, "Couldn't read from stream"))
+        }
+    }
 }
 
 impl<O: IsA<Stream>> StreamExtManual for O {
